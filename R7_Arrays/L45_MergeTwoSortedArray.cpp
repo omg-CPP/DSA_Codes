47c286d4589fbab5c1cf9c3c941162e1a605c35d
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2025-10-15 23:09:01 +0530

    Add files via upload

---
+++ R7_Arrays/L45_MergeTwoSortedArray.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void displayVector(vector<int>& v) {
+    for(int i = 0 ; i < v.size() ; i++) {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+// time Complexity : O(n+m)
+// space complexity : O(n+m)
+
+vector<int> mergeTwoSortedArrays(vector<int>& v1, vector<int>& v2) {
+
+    int m = v1.size();
+    int n = v2.size();
+    vector<int> mergedArray(m+n);
+    int i = m-1; // v1
+    int j = n-1; // v2
+    int k = m+n-1; // mergedArray
+
+    while(i >= 0 && j >= 0) {
+        if(v1[i] >= v2[j]) {
+            mergedArray[k] = v1[i];
+            i--;
+            k--;
+        }
+        else { // v1[i] < v2[j]
+            mergedArray[k] = v2[j];
+            j--;
+            k--;
+        }
+    }
+
+    if(i == -1) { // v1 is covered
+        while(j >= 0) {
+            mergedArray[k] = v2[j];
+            j--;
+            k--;
+        }
+    }
+
+    if(j == -1) { // v2 is covered
+        while(i >= 0) {
+            mergedArray[k] = v1[i];
+            i--;
+            k--;
+        }
+    }
+
+    return mergedArray;
+}
+
+int main()
+{
+    vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(4);
+    v1.push_back(5);
+    v1.push_back(8);
+
+    vector<int> v2;
+    v2.push_back(2);
+    v2.push_back(3);
+    v2.push_back(6);
+    v2.push_back(7);
+    v2.push_back(10);
+    v2.push_back(12);
+
+    displayVector(v1); // 1 4 5 8 
+
+    displayVector(v2); // 2 3 6 7 10 12 
+
+    vector<int> v = mergeTwoSortedArrays(v1,v2);
+
+    displayVector(v); // 1 2 3 4 5 6 7 8 10 12 
+    return 0;
+}
