071e686d507ac87f5ed9a8f7c5f18b224d4ca79e
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2025-10-17 23:25:11 +0530

    Add files via upload

---
+++ R7_Arrays/L55_FirstNonRepeatingElement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,2,4,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0 ; i < n ; i++) {
+        int j;
+        for(j = 0 ; j < n ; j++) {
+            if(i != j && arr[i] == arr[j]) {
+                break;
+            }
+        }
+        if(j == n) {
+            cout<<arr[i]; // 1
+            return 0;
+        }
+    }
+    return 0;
+}
