fd81e10cdcc463dcd87d1374e492725550778f27
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-01 21:16:26 +0530

    Add files via upload

---
+++ R14_LinkedList/L38_MaximumTwinSumOfALinkedList.cpp
@@ -0,0 +1,25 @@
+// Leetcode 2130
+
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;   
+    ListNode* next;
+
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+
+int pairSum(ListNode* head) {
+        
+}
+
+int main()
+{
+    return 0;
+}
