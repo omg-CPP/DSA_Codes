11b847cddd214e893963a26def56d66ab5574496
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-01 21:17:17 +0530

    Add files via upload

---
+++ R14_LinkedList/L40_SplitLinkedListInOddAndEvenValues.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public : 
+    int val;
+    ListNode* next;
+    // Constructor
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
