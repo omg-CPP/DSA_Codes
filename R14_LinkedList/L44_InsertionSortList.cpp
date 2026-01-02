d3c9deadc361dfc7b2f7cb8247b51b509d74a2ba
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-02 23:13:57 +0530

    Add files via upload

---
+++ R14_LinkedList/L44_InsertionSortList.cpp
@@ -0,0 +1,24 @@
+// Leetcode : 147
+
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;   
+    ListNode* next;
+
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+ListNode* insertionSortList(ListNode* head) {
+        
+}
+
+int main()
+{
+    return 0;
+}
