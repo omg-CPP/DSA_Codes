20d7e33735dc42ee6d62f7cf998bc16d082f344f
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-03 22:54:19 +0530

    Add files via upload

---
+++ R14_LinkedList/L49_SwapNodesInPairs.cpp
@@ -0,0 +1,24 @@
+// Leetcode : 24
+
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;   
+    ListNode* next;
+
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+ListNode* swapPairs(ListNode* head) {
+        
+}
+
+int main()
+{
+    return 0;
+}
