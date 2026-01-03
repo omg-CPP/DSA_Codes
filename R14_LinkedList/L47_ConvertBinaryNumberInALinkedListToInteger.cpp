9dca7ff4dae002654bbc4c36569a7e78c0773472
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-03 22:53:41 +0530

    Add files via upload

---
+++ R14_LinkedList/L47_ConvertBinaryNumberInALinkedListToInteger.cpp
@@ -0,0 +1,24 @@
+// Leetcode : 1290
+
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;   
+    ListNode* next;
+
+    ListNode(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int getDecimalValue(ListNode* head) {
+        
+}
+
+int main()
+{
+    return 0;
+}
