958b25a6e8fd4cd39935ba2eb4dba28266827f75
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-09 00:12:53 +0530

    Add files via upload

---
+++ R16_Stack/L10_PushAtBottomRecursive.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void displayStack(stack<int>& st) { 
+    stack<int> temp;
+    while(st.size() > 0) {
+        temp.push(st.top());
+        st.pop();
+    }
+    // moving elements back to st
+    while(temp.size() > 0) {
+        cout<<temp.top()<<" ";
+        st.push(temp.top());
+        temp.pop();
+    }
+    cout<<endl;
+}
+
+void pushAtBottom(stack<int>& st, int val) {
+    if(st.size() == 0) {
+        st.push(val);
+        return;
+    }
+    int x = st.top();
+    st.pop();
+    pushAtBottom(st,val);
+    st.push(x);
+}
+
+int main()
+{
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    displayStack(st);
+    // 10 20 30 40 50 
+    
+    pushAtBottom(st,80);
+
+    displayStack(st);
+    // 80 10 20 30 40 50 
+
+    return 0;
+}
