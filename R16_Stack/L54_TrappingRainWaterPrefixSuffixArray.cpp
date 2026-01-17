a7ad5c4a7298bd9756f1868fa1d02bac70bc50eb
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-17 23:55:16 +0530

    Add files via upload

---
+++ R16_Stack/L54_TrappingRainWaterPrefixSuffixArray.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <cmath>
+using namespace std;
+int main()
+{
+    return 0;
+}
