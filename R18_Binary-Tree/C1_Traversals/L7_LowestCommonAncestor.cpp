5d8b706b13a3c61ddc75301c9ae3730333c220d5
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-22 20:06:49 +0530

    Add files via upload

---
+++ R18_Binary-Tree/C1_Traversals/L7_LowestCommonAncestor.cpp
@@ -0,0 +1,42 @@
+// Leetcode : 236 : LCA
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TreeNode {
+public : 
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    // Constructor
+    TreeNode(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    } 
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    // base case
+    if(root == NULL || root == p || root == q) {
+        return root;
+    }
+    TreeNode* left = lowestCommonAncestor(root->left,p,q);
+    TreeNode* right = lowestCommonAncestor(root->right,p,q);
+    // result
+    if(left == NULL) {
+        return right;
+    }
+    else if(right == NULL) {
+        return left;
+    }
+    else { // left != NULL and right != NULL
+        return root;
+    }
+}
+
+int main()
+{
+    return 0;
+}
