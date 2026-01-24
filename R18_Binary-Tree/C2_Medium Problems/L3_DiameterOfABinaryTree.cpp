60d2f73edf6eb32d8a9d3dbb511ebe38688e21aa
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-24 18:48:02 +0530

    Add files via upload

---
+++ R18_Binary-Tree/C2_Medium Problems/L3_DiameterOfABinaryTree.cpp
@@ -0,0 +1,43 @@
+// Leetcode : 543
+// Diameter : Longest Path betweem two nodes
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TreeNode {
+public : 
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    // Constructor
+    TreeNode(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int levels(TreeNode* root, int& diameter) {
+    if(root == NULL) {
+        return 0;
+    }
+    int leftLevels = levels(root->left,diameter);
+    int rightLevels = levels(root->right,diameter);
+    diameter = max(diameter, leftLevels + rightLevels);
+    return 1 + max(leftLevels,rightLevels);
+}
+
+// T.C. = O(n)
+// S.C. = O(height) in best case
+// S.C. = O(n) in worst case
+int diameterOfBinaryTree(TreeNode* root) {
+    int diameter = 0;
+    levels(root,diameter);
+    return diameter;
+}
+
+int main()
+{
+    return 0;
+}
