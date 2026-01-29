066830a4a66cebf5e3efa4fa2186b96f73144e40
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-29 17:27:06 +0530

    Add files via upload

---
+++ R18_Binary-Tree/C3_Hard Problems/L13_ConstructBinaryTreeFromPreOrderAndPostOrderTraversal.cpp
@@ -0,0 +1,30 @@
+// Leetcode : 889
+
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class TreeNode {
+public : 
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    // Constructor
+    TreeNode(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// Unique Binary Tree will be formed
+TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+    
+}
+
+int main()
+{
+    return 0;
+}
