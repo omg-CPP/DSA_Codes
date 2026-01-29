f3d36c8f74042f5b9e7f86fe032ec706a6f2ce80
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-29 17:26:43 +0530

    Add files via upload

---
+++ R18_Binary-Tree/C3_Hard Problems/L12_ConstructBinaryTreeFromInOrderAndPostOrderTraversal.cpp
@@ -0,0 +1,28 @@
+// Leetcode : 106
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TreeNode {
+public : 
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    // Constructor
+    TreeNode(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        
+}
+
+int main()
+{
+    return 0;
+}
