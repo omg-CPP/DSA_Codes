1cba5284df4cc75edaa0f67907a3a196fce6e55f
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2026-01-30 23:58:00 +0530

    Add files via upload

---
+++ R18_Binary-Tree/C3_Hard Problems/L18_FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,28 @@
+// Leetcode : 114
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TreeNode {
+public : 
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    // Constructor
+    TreeNode(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void flatten(TreeNode* root) {
+    
+}
+
+int main()
+{
+    return 0;
+}
